5470632a093a7d12df309f9697e4134c3e3de57e
sustained <sustained.dissonance@gmail.com>
2019-01-10 18:40:47 +0000

    Format entry depending on exercise type.

--- src/components/LessonComplete.vue
+++ src/components/LessonComplete.vue
@@ -3,10 +3,7 @@
     <p>Lesson over!</p>
 
     <ul>
-      <li
-        v-for="(entry, index) in session"
-        :key="index"
-      >You wrote {{ entry.choice}} and were {{ entry.wasCorrect ? 'correct' : 'incorrect'}}</li>
+      <li v-for="(entry, index) in session" :key="index">{{ formatSessionEntry(entry) }}</li>
     </ul>
 
     <p>
@@ -18,6 +15,20 @@
 <script>
 export default {
   props: ["session"],
-  name: "lesson-ended"
+  name: "lesson-ended",
+  methods: {
+    // TODO: Refactor, at some point? It works for now though.
+    formatSessionEntry(entry) {
+      let answer =
+        entry.type === "select-words"
+          ? entry.choice.map(e => e.choice).join(" ")
+          : entry.choice;
+      let adjective = entry.correct ? "correct" : "incorrect";
+
+      return `For the prompt "${
+        entry.prompt
+      }", you selected "${answer}" and were ${adjective}!`;
+    }
+  }
 };
 </script>
